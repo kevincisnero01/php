41d1a199604c435fd319de6e8fbce9a25c017d5d
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2022-03-24 12:40:18 -0430

    Resumen

---
+++ calcular_edad_diofanto.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PHP</title>
+</head>
+<body>
+
+	<?php 
+
+	$age = 0;
+	$x = 0;
+	$infancia = $x/6;
+	$adulto = $x/12;
+	$matrimonio = $x/7;
+	$hijoNacimiento = 5; //quinquenio son 5 años
+	$hijoMuerte = $x/2;
+	$pena = 4;
+
+	//Programar: dar ordenes a la maquina
+	//Programa: serie de instrucciones que puede ejecutar muchas veces, con distintos datos y obteniendo distintos resultados
+	//Algoritmo: sucesicion de pasos ordenados que la computadora ejecuta
+
+
+
+	?>
+
+</body>
+</html>
